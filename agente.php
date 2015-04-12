157c45a449b2bebf04a92b19d06b2c79b2d51c4e
challengecode <info@arkux.com>
2015-04-12 06:12:10 -0600

    Create agente.php

---
+++ agente.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Desarrolladores: Arelys Orozco
+						 Arnoldo Quirós
+						 Ariel Campos
+						 Joseph Loaiza						 
+Hackathon 2015 
+Coffe Cloud
+*/
+include("conexion.php");//llamamos la clase de conexion
+$db = new MySQL();//instanciar la clase
+$i=0;//contador...
+$respuesta = array();//array que enviara respuesta
+$provincia = $_REQUEST['provincia'];
+$consulta = $db->consulta("SELECT ID,NOMBRE FROM AGENTES  ");
+	if($db->num_rows($consulta)>0){
+			$paso=1;//bandera de que si tiene datos;
+			  while($resultados = $db->fetch_array($consulta)){ 
+				   $name=$resultados['NOMBRE'];
+					$id=$resultados['ID'];				
+					$respuesta[$i]['nombre'] = $name;
+					$respuesta[$i]['id'] = $id;
+					$i++;
+			 }
+			 $dataset['resultado'] = $respuesta;
+		}
+		$resultadosJson = Json_encode($dataset);
+		echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
+?>
